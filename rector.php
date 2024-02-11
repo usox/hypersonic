c9cc23b44a3e26fb38fcfc209c4ce7fae45e3662
Daniel Jakob <github@usox.org>
2024-02-11 12:30:33 +0100

    Update dependencies

--- rector.php
+++ rector.php
@@ -5,25 +5,17 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ]);
-
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    $rectorConfig->sets([
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::DEAD_CODE,
-        LevelSetList::UP_TO_PHP_81,
-    ]);
-
-    $rectorConfig->skip([
-       FlipTypeControlToUseExclusiveTypeRector::class,
-    ]);
-};
+return RectorConfig::configure()
+	->withPaths([
+		__DIR__ . '/src',
+		__DIR__ . '/tests',
+	])
+	->withRules([
+		InlineConstructorDefaultToPropertyRector::class,
+	])
+	->withPhpSets(php81: true)
+	->withPreparedSets(deadCode: true, codeQuality: true, codingStyle: true)
+	->withSkip([
+		FlipTypeControlToUseExclusiveTypeRector::class,
+	]);
